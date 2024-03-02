a3565c70e5312eee820f6110b7103f42e2e562af
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-03-02 10:43:31 -0500

    Add Data Component to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
+import { Data } from "./components/Data";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <>
-        Hola Despliegue en la Nube
+        Hola Despliegue en la Nube:
+        <hr/>
+        <Data/>
     </>
 )
